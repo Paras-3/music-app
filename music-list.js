6ae69bcd4048931e55aa3b978d97258b14167d55
Paras-3 <90415163+Paras-3@users.noreply.github.com>
2021-09-22 23:54:42 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,76 @@
+let allMusic = [
+  {
+    name: "Raataan Lambiyan   Shershaah",
+    artist: "Jubin Nautiyal , Asees Kaur",
+    img: "C:\Users\paras\Desktop\mini-project newton\Music Player\images\raataan-lambiyan-shershaah",
+    src: "Raataan Lambiyan - Shershaah"
+  },
+  {
+    name: "Ranjha    Shershaah",
+    artist: "B Praak",
+    img: "ranjha-shershaah.jpg",
+    src: "Ranjha - Shershaah"
+  },
+  {
+    name: "Man Bharya  Shershaah ",
+    artist: "B Praak",
+    img: "man-bharya",
+    src: "Man Bharya 2"
+  },
+  {
+    name: "Param Sundari  Mimi",
+    artist: "Shreya Ghoshal",
+    img: "param-sundari-mimi",
+    src: "Param Sundari - Mimi"
+  },
+  {
+    name: "Sakhiyan 2  Bell Bottom",
+    artist: "Manidar Buttar ,Zara Khan",
+    img: "sakhiyan-2-bell-bottom",
+    src: "Sakhiyan 2 - Bell Bottom"
+  },
+  {
+    name: "Waalian  Harnoor",
+    artist: "Harnoor",
+    img: "waalian-harnoor",
+    src: "Waalian - Harnoor"
+  },
+  {
+    name: "Brown Munde",
+    artist: "AP DHILLON , GURINDER GILL , SHINDA KAHLON",
+    img: "brown-munde",
+    src: "Brown Munde"
+  },
+  {
+    name: "Pani Pani ",
+    artist: "Badshah, Aastha Gill ",
+    img: "saiyaan-ne-dekha-aise-main-pani-pani-ho-gayi",
+    src: "Saiyaan Ne Dekha Aise Main Pani Pani Ho Gayi"
+  },
+  {
+    name: "Kanta Laga ",
+    artist: "Tony Kakkar, Yo Yo Honey Singh & Neha Kakkar",
+    img: "kanta-laga-yo-yo-honey-singh",
+    src: "Kanta Laga - Yo Yo Honey Singh"
+  },
+  {
+    name: "Knock Knock Tera Baap Aaya",
+    artist: "Farhad Bhiwandiwala",
+    img: "knock-knock-tera-baap-aaya",
+    src: "Knock Knock Tera Baap Aaya"
+  },
+ 
+];
+
+
+
+//To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - img must be in .jpg format and it's inside the images folder of this project folder",
+//     src: "music name here - song must be in .mp3 format and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
